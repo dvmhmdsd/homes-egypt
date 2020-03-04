c5f8a47b2eb0ee77d0ae0d26fa0c828bae910f3f
mohamedsaad <mahameds19999@gmail.com>
2020-03-04 21:50:46 +0200

    Contract Us page show success message

--- src/homes/modules/shared/components/contact-form/contact-form.component.js
+++ src/homes/modules/shared/components/contact-form/contact-form.component.js
@@ -1,30 +1,40 @@
 class ContactForm {
-    /**
-     * Constructor
-     * Put your required dependencies in the constructor parameters list  
-     */
-    constructor(formSubmitService) {
-        this.formSubmitService = formSubmitService;
-    }
-    
-    /**
-     * Initialize the component
-     * This method is triggered before rendering the component
-     */
-    init() {}
+  /**
+   * Constructor
+   * Put your required dependencies in the constructor parameters list
+   */
+  constructor(formSubmitService) {
+    this.formSubmitService = formSubmitService;
+  }
 
-    async send(form) {
-        this.isLoading = true;
+  /**
+   * Initialize the component
+   * This method is triggered before rendering the component
+   */
+  init() {}
 
-        await this.formSubmitService.contactUs(form);
+  async send(form) {
+    this.isLoading = true;
 
-        this.isLoading = false;
+    this.submitSuccess = false;
 
+    try {
+      await this.formSubmitService.contactUs(form);
+
+      this.isLoading = false;
+
+      this.submitSuccess = true;
+
+      setTimeout(() => {
         this.modal.close();
+      }, 1500);
+    } catch (err) {
+      throw err;
     }
+  }
 
-    /**
-     * The component is ready to do any action after being rendered in dom
-     */
-    ready() {}
+  /**
+   * The component is ready to do any action after being rendered in dom
+   */
+  ready() {}
 }
